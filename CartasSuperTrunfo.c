ecc6622fd0faa8298a319719c7daa67c5a65f81a
Hermesson Yuri <hermessonyuri@hotmail.com>
2025-06-20 18:31:32 +0000

    Update CartaSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,60 @@
-#include <stdio.h>
+#include <stdio.h> 
+// Biblioteca  para entrada e saída (printf, scanf)
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+/*
+ * Tarefa da Faculdade
+ * DISCIPLINA: PROGRAMAÇÃO EM C
+ *
+ * Desafio: Super Trunfo = Nível Novato
+ * Objetivo: Cadastrar e exibir cartas para o jogo Super Trunfo com dados de cidades
+ * Autor: Hermesson Yuri
+ * Data: 19/06/2025 Finalizei em 20/06/2025
+ * GitHub: https://github.com/hermessonyurii
+ *
+ */
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+ int main(){
+
+    // CARTA 1: Alagoas
+    // Variáveis da Carta 1
+    char estado1 = 'A'; //  Estado: Alagoas
+    char codigo1[4] = "A01"; // Código da Carta
+    char cidade1[] = "Maceio"; // Nome da Cidade
+    int populacao1 = 957916; // População Fonte "https://www.ibge.gov.br/cidades-e-estados/al/maceio.html"
+    float area1 =  509.6f; // Área em km² Fonte "https://www.ibge.gov.br/cidades-e-estados/al/maceio.html"
+    double pib1 = 27484016310.31; // PIB em R$ Fonte "https://maceio.al.gov.br/noticias/sefaz/maceio-registra-maior-crescimento-do-pib-entre-as-capitais-do-nordeste"
+    int pontos_turisticos1 = 15; // Pontos Turísticos Fonte "https://www.tripadvisor.com.br/Attractions-g303216-Activities-Maceio_State_of_Alagoas.html"
+
+    // Exibindo a Carta 1
+    printf("\nCarta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Codigo: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", cidade1);
+    printf("Populacao: %d  habitantes\n", populacao1);
+    printf("Area: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1 / 1000000000.0f);
+    printf("Numero de Pontos Turísticos: %d\n", pontos_turisticos1);
+
+    // CARTA 2: Santa Catarina
+    // Variáveis da Carta 2
+    char estado2 = 'B'; // Estado: Santa Catarina
+    char codigo2[4] = "B02"; // Código da Carta 
+    char cidade2[] = "Florianopolis"; // Nome da Cidade
+    int populacao2 = 7610361; // População Fonte "https://cidades.ibge.gov.br/brasil/sc/panorama"
+    float area2 = 674.844f; // Aréa em km² Fonte "https://www.ibge.gov.br/cidades-e-estados/sc/florianopolis.html"
+    double pib2 = 65296490000.0f; // PIB em R$ Fonte "https://sctd.com.br/economia/sc-lidera-crescimento-do-pib-com-alta-de-69/#:~:text=Apenas%20nos%20primeiros%20quatro%20meses,US%5C%24%2011%2C9%20bilh%C3%B5es."
+    int pontos_turisticos2 = 15; // Pontos Turísticos Fonte "https://www.tripadvisor.com.br/Attractions-g303570-Activities-State_of_Santa_Catarina.html"
+
+
+    // Exibindo a Carta 2
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Codigo: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", cidade2);
+    printf("Populacao: %d habitantes\n", populacao2);
+    printf("Area: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2 / 1000000000.0f);
+    printf("Numero de Pontos Turísticos: %d\n", pontos_turisticos2);
 
     return 0;
-}
+ }
